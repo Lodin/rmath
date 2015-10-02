23bcf411b24fbdf2fc9c9dad1f5cd172b0977a5c
Vlad Rindevich <rindevich.vs@gmail.com>
2015-10-02 16:04:39 +0600

    Add MatrixSquare tests

--- tests/test_matrix.rs
+++ tests/test_matrix.rs
@@ -1,13 +1,30 @@
 extern crate rmath;
-use rmath::types::matrix::{ Matrix, Matrix3x3 };
+use rmath::types::matrix::{ Matrix, MatrixSquare, Matrix3x3 };
 
 #[test]
-fn test_matrix_basics() {
-    let mat: Matrix3x3<i32> = Matrix3x3::new_filled(&[
+fn test_matrix_creation_simple() {
+    let mat = Matrix3x3::<i32>::new_filled(&[
          3, -1, 6,
          2,  1, 5,
         -3,  1, 0
     ]);
 
     assert_eq!(mat[2][0], -3);
+}
+
+#[test]
+fn test_matrix_creation_square() {
+    let mat = Matrix3x3::<i32>::new_identity();
+
+    assert_eq!(mat[0][0], 1);
+    assert_eq!(mat[0][1], 0);
+    assert_eq!(mat[1][0], 0);
+    assert_eq!(mat[1][1], 1);
+
+    let mat2 = Matrix3x3::<i32>::new_diag(&[1, 2, 3]);
+
+    assert_eq!(mat2[0][0], 1);
+    assert_eq!(mat2[0][1], 0);
+    assert_eq!(mat2[1][0], 0);
+    assert_eq!(mat2[1][1], 2);
 }
