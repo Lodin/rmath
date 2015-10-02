c4598c41a910adbf69a9fc3f98a9bfc372cdb6cb
Vlad Rindevich <rindevich.vs@gmail.com>
2015-10-02 11:20:38 +0600

    Add basic tests

--- tests/test_matrix.rs
+++ tests/test_matrix.rs
@@ -1,6 +1,13 @@
-use rmath::types::matrix::Matrix3x3;
+extern crate rmath;
+use rmath::types::matrix::{ Matrix, Matrix3x3 };
 
 #[test]
 fn test_matrix_basics() {
-    let mat = Matrix3x3::<i32>::new_filled();
+    let mat: Matrix3x3<i32> = Matrix3x3::new_filled(&[
+         3, -1, 6,
+         2,  1, 5,
+        -3,  1, 0
+    ]);
+
+    assert_eq!(mat[2][0], -3);
 }
